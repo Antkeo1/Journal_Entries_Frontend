6b8bd3379335c77c346df048d9e41401a71f8f5a
Antkeo1 <Antonio.keo@ieleadership.org>
2018-11-26 13:13:18 -0500

    updating the front end of the project

---
+++ assets/scripts/api.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const config = require('./config.js')
+const store = require('./store.js')
+
+const signUp = data => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data
+  })
+}
+
+const signIn = data => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-in',
+    method: 'POST',
+    data
+  })
+}
+
+const changePassword = data => {
+  return $.ajax({
+    url: config.apiUrl + '/change-password',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const signOut = () => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-out',
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const createJournal = function () {
+  return $.ajax({
+    url: config.apiUrl + '/journals',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const saveJournal = data => {
+  return $.ajax({
+    url: config.apiUrl + '/journals/:id',
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword,
+  signOut,
+  createJournal,
+  saveJournal
+}
