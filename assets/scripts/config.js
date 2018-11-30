6e0f49d255f435cc8e94a075d8189009f02a8fb4
Antkeo1 <Antonio.keo@ieleadership.org>
2018-11-30 09:06:16 -0500

    fixed production link on config

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://shrouded-chamber-38593.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
